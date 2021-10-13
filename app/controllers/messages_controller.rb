0036aad3dc7059dde3b439166a24249f78858bd2
Quentin Peschard <qpeschard49@gmail.com>
2021-10-13 17:41:37 +0200

    ok message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,4 +9,10 @@ class MessagesController < ApplicationController
       render 'chatrooms/show'
     end
   end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
