5cc0da40e9f717c7d0876732d3cfacd7589cfcc2
Quentin Peschard <qpeschard49@gmail.com>
2021-10-14 17:28:16 +0200

    another try

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -3,4 +3,8 @@ class ChatroomChannel < ApplicationCable::Channel
     chatroom = Chatroom.maximum(:chat_id)
     stream_for chatroom
   end
+
+  def receive(data)
+    ActionCable.server.broadcast(chatroom, data)
+  end
 end
